693f16b83f2b1b85e3f8fd4aafa805ac10915685
TechWithTyler <twsandmds@gmail.com>
2023-02-27 13:30:20 -0800

    Fixed layout

--- Skippy/ViewController.swift
+++ Skippy/ViewController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
 
 	@IBOutlet weak var questionLabel: UILabel!
 
-	@IBOutlet weak var objectImageView: UIImageView!
+	@IBOutlet weak var objectCollectionView: UICollectionView!
 
 	@IBOutlet weak var choice1Button: UIButton!
 
@@ -27,6 +27,8 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 		newQuestion()
+		objectCollectionView.dataSource = self
+		objectCollectionView.delegate = self
 	}
 
 	func newQuestion() {
@@ -34,7 +36,6 @@ class ViewController: UIViewController {
 		questionLabel.text = gameBrain.getQuestionText()
 		setColors()
 		setChoices()
-		showGroupsOfObjects(object: gameBrain.currentObject, total: gameBrain.numberOfObjectsToShow, groups: gameBrain.groupsOfObjectsToShow)
 	}
 
 	@IBAction func answerSelected(_ sender: UIButton) {
@@ -47,34 +48,6 @@ class ViewController: UIViewController {
 		}
 	}
 
-	func showGroupsOfObjects(object: Object, total: Int, groups: Int) {
-		guard let image = UIImage(named: object.imageName) else { return }
-		let objectsPerGroup = total/groups
-		let screenWidth = objectImageView.bounds.width
-		let maxObjectsPerRow = Int(screenWidth / (image.size.width + 10))
-		let objectsPerRow = min(objectsPerGroup, maxObjectsPerRow)
-		let rowsPerGroup = Int(ceil(Double(objectsPerGroup) / Double(objectsPerRow)))
-		for groupIndex in 0..<groups {
-			let startingIndex = groupIndex * objectsPerGroup + 1
-			let endingIndex = min((groupIndex + 1) * objectsPerGroup, total)
-			let objectsInGroup = endingIndex - startingIndex + 1
-			let rowsInGroup = Int(ceil(Double(objectsInGroup) / Double(objectsPerRow)))
-			let groupWidth = CGFloat(min(objectsInGroup, objectsPerRow)) * (image.size.width + 10) - 10
-			let groupOffsetX = (screenWidth - groupWidth) / 2.0
-			let groupOffsetY = CGFloat(groupIndex * rowsPerGroup) * (image.size.height + 10)
-			for i in startingIndex...endingIndex {
-				let newImageView = UIImageView(image: image)
-				let objectIndex = i - startingIndex
-				let row = objectIndex / objectsPerRow
-				let col = objectIndex % objectsPerRow
-				let x = groupOffsetX + CGFloat(col) * (image.size.width + 10)
-				let y = groupOffsetY + CGFloat(row) * (image.size.height + 10)
-				newImageView.frame = CGRect(x: x, y: y, width: image.size.width, height: image.size.height)
-				objectImageView.addSubview(newImageView)
-			}
-		}
-	}
-
 	func setChoices() {
 		choice1Button.setTitle("2", for: .normal)
 		choice2Button.setTitle("4", for: .normal)
@@ -84,18 +57,53 @@ class ViewController: UIViewController {
 
 	func setColors() {
 		view.backgroundColor = gameBrain.getColors().background
+		objectCollectionView.backgroundColor = gameBrain.getColors().background
 		choice1Button.tintColor = gameBrain.getColors().buttons
 		choice2Button.tintColor = gameBrain.getColors().buttons
 		choice3Button.tintColor = gameBrain.getColors().buttons
 		choice4Button.tintColor = gameBrain.getColors().buttons
 	}
 
-	func clearImageView() {
-		for view in objectImageView.subviews {
-			view.removeFromSuperview()
-		}
+	func numberOfSections(in collectionView: UICollectionView) -> Int {
+		let number = gameBrain.numberOfObjectsToShow / gameBrain.groupsOfObjectsToShow
+		print("Number of rows: \(number)")
+		return number
+	}
+
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		let number = gameBrain.groupsOfObjectsToShow
+		print("Number of objects in row \(section): \(number)")
+		return number
+	}
+
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ObjectCell", for: indexPath)
+		// Create and configure the image view
+		let imageView = UIImageView(frame: cell.contentView.bounds)
+		imageView.image = UIImage(named: gameBrain.currentObject.imageName)
+		imageView.contentMode = .scaleAspectFit
+		imageView.clipsToBounds = true
+		imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+		imageView.center = cell.contentView.center
+		// Add the image view to the cell's content view
+		cell.contentView.addSubview(imageView)
+		return cell
+	}
+
+
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+		return 20
+	}
+
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		let numberOfObjectsInRow = gameBrain.groupsOfObjectsToShow
+		let availableWidth = collectionView.bounds.width - collectionView.contentInset.left - collectionView.contentInset.right
+		let cellWidth = (availableWidth - CGFloat(numberOfObjectsInRow - 1) * 15) / CGFloat(numberOfObjectsInRow)
+		let numberOfRows = ceil(CGFloat(gameBrain.numberOfObjectsToShow) / CGFloat(numberOfObjectsInRow))
+		let availableHeight = collectionView.bounds.height - collectionView.contentInset.top - collectionView.contentInset.bottom
+		let cellHeight = (availableHeight - CGFloat(numberOfRows - 1) * 15) / CGFloat(numberOfRows)
+		return CGSize(width: cellWidth, height: cellHeight)
 	}
 
 
 }
-
