b12daec165d7dc0aad217cec00d7064951dede3b
TechWithTyler <twsandmds@gmail.com>
2024-04-29 13:35:42 -0700

    Updated max groups picker UI
    Picker row height and text size has been increased.
    Picker rows now use the same color as buttons.

--- SkippyNums/UI and Images/View Controllers/WelcomeViewController.swift
+++ SkippyNums/UI and Images/View Controllers/WelcomeViewController.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2023-2024 SheftApps. All rights reserved.
 //
 
-import UIKit
+import SheftAppsStylishUI
 
 class WelcomeViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
@@ -38,7 +38,7 @@ class WelcomeViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
 		gradientLayer.colors = traitCollection.userInterfaceStyle == .dark ? gradientColorsDark : gradientColorsLight
 		gradientLayer.startPoint = CGPoint(x: 0.5, y: 1)
 		gradientLayer.endPoint = CGPoint(x: 0.5, y: 0)
-		configureRowsPicker()
+		configureMaxGroupsPicker()
 		// Add gradient layer to view
 		view.layer.insertSublayer(gradientLayer, at: 0)
 	}
@@ -86,7 +86,7 @@ extension WelcomeViewController {
 
 	// MARK: - Max Groups Picker - Configuration
 
-	func configureRowsPicker() {
+	func configureMaxGroupsPicker() {
         maxGroupsPicker?.isAccessibilityElement = true
 		maxGroupsPicker?.accessibilityLabel = "Maximum number of groups"
 		maxGroupsPicker?.delegate = self
@@ -96,28 +96,43 @@ extension WelcomeViewController {
 
     // MARK: - Max Groups Picker - Delegate and Data Source
 
+    // Returns the number of components (wheels) for the picker.
 	func numberOfComponents(in pickerView: UIPickerView) -> Int {
 		return 1
 	}
 
+    // Returns the number of rows for a given component (wheel) in the picker.
 	func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
 		return 2
 	}
 
+    // Handles selection of picker rows on the given wheel.
 	func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
 		settingsData.tenFrame = row == 1 ? true : false
 	}
 
+    // Returns the content to display for the given row on the given wheel.
 	func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        // 1. Create the label for the row.
 		var pickerLabel: UILabel? = (view as? UILabel)
 		if pickerLabel == nil {
 			pickerLabel = UILabel()
-			pickerLabel?.font = UIFont(name: "Helvetica", size: 30)
+			pickerLabel?.font = UIFont(name: "Helvetica", size: 40)
 			pickerLabel?.textAlignment = .center
-		}
+            pickerLabel?.layer.cornerRadius = 12
+        }
+        // 2. Get the title for the label based on the row being configured.
         let optionTitle = String(maxGroupsOptions[row])
-		pickerLabel?.text = optionTitle
+        pickerLabel?.text = optionTitle
+        // 3. Set the background color of the row.
+        pickerLabel?.layer.backgroundColor = UIColor.tintColor.cgColor
+        // 4. Return the label as the picker item's view.
 		return pickerLabel!
 	}
+
+    // Returns the row height for the picker.
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 40
+    }
 
 }
